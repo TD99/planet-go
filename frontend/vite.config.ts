260f0636bf2ef276573ea4b2666dcdf2d551a83b
Tim Dürr <tim@duerrs.ch>
2023-05-09 12:12:46 +0200

    Init2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,24 @@
-import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate'
+  })],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+  },
+  resolve: {
+    alias: {
+      '@src': '/src',
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@theme': '/src/theme',
+      '@lib': '/src/lib'
+    }
   }
 })
