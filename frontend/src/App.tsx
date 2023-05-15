cfc55f5c8b206cae853a9cb657e5f43ac4df027e
Tim Dürr <tim@duerrs.ch>
2023-05-15 08:46:20 +0200

    Router test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,50 @@
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, setupIonicReact } from '@ionic/react';
+import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, setupIonicReact, useIonViewDidEnter } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { home as homeIcon, mapOutline as mapIcon } from 'ionicons/icons';
 import { Home, Map } from '@pages/.';
 import './assetsImports';
+import { useEffect, useState } from 'react';
 
 setupIonicReact();
 
 const App: React.FC = () => {
+  const [showTabBar, setShowTabBar] = useState(true);
+
+  function renderRoutes() {
+    return (
+      <IonRouterOutlet>
+        <Redirect exact path="/" to="/home" />
+        <Route exact path="/home" render={() => <Home />} />
+        <Route exact path="/game/map" render={() => <Map />} />
+      </IonRouterOutlet>
+    )
+  }
+
   return (
     <IonApp>
       <IonReactRouter>
-        <IonTabs>
-          <IonRouterOutlet>
-            <Redirect exact path="/" to="/home" />
-            <Route exact path="/home" render={() => (<Home />)} />
-            <Route exact path="/game/map" render={() => (<Map />)} />
-          </IonRouterOutlet>
+        {
+          showTabBar ? (
+          <IonTabs>
+            {
+              renderRoutes()
+            }
 
-          <IonTabBar slot='bottom'>
-            <IonTabButton tab="home" href='/home'>
-              <IonIcon icon={homeIcon} />
-              <IonLabel>Home</IonLabel>
-            </IonTabButton> 
-            <IonTabButton tab="map" href='/game/map'>
-              <IonIcon icon={mapIcon} />
-              <IonLabel>Map</IonLabel>
-            </IonTabButton> 
-          </IonTabBar>
-        </IonTabs>
+            <IonTabBar slot='bottom'>
+              <IonTabButton tab="home" href='/home'>
+                <IonIcon icon={homeIcon} />
+                <IonLabel>Home</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="map" href='/game/map'>
+                <IonIcon icon={mapIcon} />
+                <IonLabel>Map</IonLabel>
+              </IonTabButton>
+            </IonTabBar>
+          </IonTabs>
+        ) : (
+          renderRoutes()
+        )}
       </IonReactRouter>
     </IonApp>
   )
